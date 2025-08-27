308da4f16ef09c68c3fec73bf6291f7d7b98eef8
Duryodhan <hackwithrock@gmail.com>
2025-08-27 16:18:44 +0530

    Update Numbercheck.py

--- main/Numbercheck.py
+++ main/Numbercheck.py
@@ -34,8 +34,9 @@ def search_google(query):
 
 def validate_phone_number(number):
     try:
-        API_KEY = "6cddf1fcd79d985e9efc9e140424b888"
-        url = "https://apilayer.net/api/validate"
+		url = "https://apilayer.net/api/validate"
+        api = input(f"Enter Your http://numverify.com API KEY:- ")
+        API_KEY = f"{api}"
         querystring = {"access_key": API_KEY, "number": number, "country_code": "IN", "format": "1"}
 
         print("[red][+] Validating phone number...[/red]\n")
@@ -98,4 +99,5 @@ if __name__ == "__main__":
     look(query)
     WhatsappInfo(query)
     instafind(query)
+
 
